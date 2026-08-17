2d542576569d2152241856dafecd5e44f899bbb1
Yoocodehash <lukaszkucharczyk263@gmail.com>
2026-08-17 23:56:42 +0200

    Main

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("The Player Has Now Quit The Game");
+    }
+
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("The Player Has Now Quit The Game");
+    }
+
+}
